797736725f0a7db6bc0439bdb43c20aeecf99395
Jéferson <buenojeferson@outlook.com>
2018-02-08 09:50:35 -0200

    Validar formato antes de tentar converter

--- SimpleDateTimeExtensions.cs
+++ SimpleDateTimeExtensions.cs
@@ -7,8 +7,8 @@ namespace JefersonBueno
         public static CwiSimpleDateTime FromDateTimeString(string data)
         {
             var split = data.Split(' ');
-            var (year, month, day) = FromDateString(split[0]);      
-            var (hour, minute) = FromTimeString(split[1]);
+            var (year, month, day) = FromDateString(split[0]);
+            var (hour, minute) = split.Length == 1 ? (0, 0) : FromTimeString(split[1]);
             
             return new CwiSimpleDateTime(year, month, day, hour, minute);
         }
@@ -18,7 +18,7 @@ namespace JefersonBueno
             var splittedDate = date.Split('/');
             
             if(splittedDate.Length != 3) 
-                return (0, 0, 0);
+                throw new FormatException($"{date} não está no formato dd/MM/yyyy");
                 
             return (Convert.ToInt32(splittedDate[2]), Convert.ToInt32(splittedDate[1]), Convert.ToInt32(splittedDate[0]));
         }
@@ -28,7 +28,7 @@ namespace JefersonBueno
             var splittedTime = time.Split(':');
             
             if(splittedTime.Length != 2)
-                return (0, 0);
+                throw new FormatException($"{time} não está no formato HH:mm");
             
             return (Convert.ToInt32(splittedTime[0]), Convert.ToInt32(splittedTime[1]));
         }
